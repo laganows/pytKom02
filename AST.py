5f4c26499399af322702137e0bb825b349fffe96
KamilBugno <bugno@student.agh.edu.pl>
2015-11-05 18:18:31 +0100

    AST

--- AST.py
+++ AST.py
@@ -42,14 +42,22 @@ class Declaration(Node):
         self.error = error
 
 class Inits(Node):
-    def __init__(self, inits, init ):
-        self.inits = inits
-        self.init = init
+    def __init__(self, inits, init):
+        self.inits = []
+        if inits:
+            self.inits.extend(inits.inits)
+        if init:
+            self.inits.append(init)
+
+
 
 class Instructions(Node):
-     def __init__(self, instructions, instruction):
-         self.instructions = instructions
-         self.instruction = instruction
+    def __init__(self, instructions, instruction):
+        self.instructions = []
+        if instructions:
+            self.instructions.extend(instructions.instructions)
+        if instruction:
+            self.instructions.append(instruction)
 
 class Condition(Node):
     pass
@@ -91,31 +99,104 @@ class Else(Node):
     def __init__(self, statement):
         self.statement = statement
 
+class RepeatUntil(Instruction):
+    def __init__(self, statement, cond):
+        self.cond = cond
+        self.statement = statement
+
+class Return(Instruction):
+    def __init__(self, expression):
+        self.expression = expression
+
+class Continue(Instruction):
+    pass
+
+
+class Break(Instruction):
+    pass
+
+class Compound(Instruction):
+    def __init__(self, declarations, instructions):
+        self.declarations = declarations
+        self.instructions = instructions
+
+
+class Condition(Node):
+    pass
+
+
+
+
+class Expression(Condition):
+    pass
+
+
+class Const(Expression):
+    def __init__(self, value):
+        self.value = value
+
+
+class Id(Expression):
+    def __init__(self, id):
+        self.id = id
+
+class BinExpr(Expression):
+    def __init__(self, left, op, right):
+        self.expr1 = left
+        self.operator = op
+        self.expr2 = right
+
+class ExpressionInPar(Expression):
+    def __init__(self, expression, error):
+        self.expression = expression
+        self.error = error
+
+class IdWithPar(Expression):
+    def __init__(self, id, expression_list, error):
+        self.id = id
+        self.expression_list = expression_list
+        self.error = error
+
+class ExpressionList(Node):
+    def __init__(self, expr_list, expression):
+        self.expressions = []
+        if expr_list:
+            self.expressions.extend(expr_list.expressions)
+        if expression:
+            self.expressions.append(expression)
+
 class Const(Node):
     pass
-    #...
 
-class Integer(Const):
-    pass
-    #...
+class FunctionDefinitions(Node):
+    def __init__(self, fundef, fundefs):
+        self.fundefs = []
+        if fundef:
+            self.fundefs.append(fundef)
+        if fundefs:
+            self.fundefs.extend(fundefs.fundefs)
+
+class FunctionDefinition(Node):
+    def __init__(self, type, id, arglist, compound_instr):
+        self.type = type
+        self.id = id
+        self.arglist = arglist
+        self.compound_instr = compound_instr
+
+class ArgumentList(Node):
+    def __init__(self, arg_list, arg):
+        self.arg_list = []
+        if arg_list:
+            self.arg_list.extend(arg_list.arg_list)
+        if arg:
+            self.arg_list.append(arg)
+
+class Argument(Node):
+    def __init__(self, type, id):
+        self.type = type
+        self.id = id
 
 
-class Float(Const):
-    pass
-    #...
 
 
-class String(Const):
-    pass
-    #...
-
-
-class Variable(Node):
-    pass
-    #...
-
-
-
-
-# ...
 
